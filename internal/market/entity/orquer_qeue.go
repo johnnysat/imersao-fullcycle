5a1912e2d09e3b0151fb5dcd8bbd11044126fc5d
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-06-22 21:46:36 -0300

    Adicionando método Less

--- internal/market/entity/orquer_qeue.go
+++ internal/market/entity/orquer_qeue.go
@@ -1,1 +1,18 @@
 package entity
+
+type OrderQeue struct {
+	Orders []*Order
+}
+
+// Método Less
+func (oq OrderQeue) Less(i, j int) bool {
+	return oq.Orders[i].Price < oq.Orders[j].Price
+}
+
+//Método Swap
+
+//Método Len
+
+// Método Push
+
+// Método Pop
