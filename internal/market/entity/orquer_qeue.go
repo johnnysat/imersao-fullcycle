4455f549cfcd4be3beec93e1263395839e443de9
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-06-26 18:03:14 -0300

    Adicionado método Poppy e corrigo em todos os métodos para adicionar o ponteiro *

--- internal/market/entity/orquer_qeue.go
+++ internal/market/entity/orquer_qeue.go
@@ -5,20 +5,23 @@ type OrderQeue struct {
 }
 
 // Método Less
-func (oq OrderQeue) Less(i, j int) bool {
+func (oq *OrderQeue) Less(i, j int) bool {
 	return oq.Orders[i].Price < oq.Orders[j].Price
 }
 
 // Método Swap
-func (oq OrderQeue) Swap(i, j int) {
+func (oq *OrderQeue) Swap(i, j int) {
 	oq.Orders[i], oq.Orders[j] = oq.Orders[j], oq.Orders[i]
 }
 
 // Método Len
-func (oq OrderQeue) Len() int {
+func (oq *OrderQeue) Len() int {
 	return len(oq.Orders)
 }
 
 // Método Push
+func (oq *OrderQeue) Push(x interface{}) {
+	oq.Orders = append(oq.Orders, x.(*Order))
+}
 
 // Método Pop
